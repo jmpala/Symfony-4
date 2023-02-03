53589b2287f8cf03119c84c2eecadff4ad1cf00a
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-02-03 14:20:40 +0100

    Chapter 22

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\TagRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
@@ -27,14 +25,9 @@ class Tag
      */
     private $name;
 
-    /**
-     * @ORM\ManyToMany(targetEntity=Question::class, mappedBy="tags")
-     */
-    private $questions;
-
     public function __construct()
     {
-        $this->questions = new ArrayCollection();
+        $this->createdAt = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -54,30 +47,4 @@ class Tag
         return $this;
     }
 
-    /**
-     * @return Collection<int, Question>
-     */
-    public function getQuestions(): Collection
-    {
-        return $this->questions;
-    }
-
-    public function addQuestion(Question $question): self
-    {
-        if (!$this->questions->contains($question)) {
-            $this->questions[] = $question;
-            $question->addTag($this);
-        }
-
-        return $this;
-    }
-
-    public function removeQuestion(Question $question): self
-    {
-        if ($this->questions->removeElement($question)) {
-            $question->removeTag($this);
-        }
-
-        return $this;
-    }
 }
