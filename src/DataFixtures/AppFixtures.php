577b224aec7dc0a51d7a8ce132313d9b975c066f
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-02-03 13:10:39 +0100

    Chapter 19

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
+use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,7 +13,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $question = QuestionFactory::createMany(20);
+        TagFactory::createMany(100);
+
+        $question = QuestionFactory::createMany(20, function () {
+            return [
+                'tags' => TagFactory::randomRange(0, 5),
+            ];
+        });
 
         QuestionFactory::new()
             ->unpublished()
@@ -35,19 +42,6 @@ class AppFixtures extends Fixture
             ->needsApproval()
             ->many(20)
             ->create();
-
-        $question = QuestionFactory::createOne();
-
-        $tag1 = new Tag();
-        $tag1->setName('dino');
-        $tag2 = new Tag();
-        $tag2->setName('dino');
-
-        $question->addTag($tag1);
-        $question->addTag($tag2);
-
-        $manager->persist($tag1);
-        $manager->persist($tag2);
 
         $manager->flush();
     }
