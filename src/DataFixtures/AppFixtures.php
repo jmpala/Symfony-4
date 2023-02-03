399c0df42061b0fd15d76e81d45cb8a0dd8d3273
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-02-03 12:12:38 +0100

    Chapter 18

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -34,6 +35,19 @@ class AppFixtures extends Fixture
             ->needsApproval()
             ->many(20)
             ->create();
+
+        $question = QuestionFactory::createOne();
+
+        $tag1 = new Tag();
+        $tag1->setName('dino');
+        $tag2 = new Tag();
+        $tag2->setName('dino');
+
+        $question->addTag($tag1);
+        $question->addTag($tag2);
+
+        $manager->persist($tag1);
+        $manager->persist($tag2);
 
         $manager->flush();
     }
