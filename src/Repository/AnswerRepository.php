edd9ccaaaec0e920f4697f88c982e8826c124a20
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-02-03 10:48:00 +0100

    Chapter 15

---
+++ src/Repository/AnswerRepository.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Answer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Query\QueryException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Answer>
+ *
+ * @method Answer|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Answer|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Answer[]    findAll()
+ * @method Answer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AnswerRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Answer::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Answer $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Answer $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public static function createApprovedCriteria(): Criteria
+    {
+        return Criteria::create()
+            ->where(Criteria::expr()->eq('status', Answer::STATUS_APPROVED));
+    }
+
+    /**
+     * @return Answer[]
+     * @throws QueryException
+     */
+    public function findMostPopular(String $search = null): array
+    {
+        $queryBuilder = $this->createQueryBuilder('answer')
+            ->addCriteria(self::createApprovedCriteria())
+            ->orderBy('answer.votes', 'DESC')
+            ->innerJoin('answer.question', 'question')
+            ->addSelect('question');
+
+        if ($search) {
+            $queryBuilder->andWhere('question.question LIKE :search or answer.content LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        return $queryBuilder->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+    }
+
+    // /**
+    //  * @return Answer[] Returns an array of Answer objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Answer
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
